d31ccb82064bdd2947b42141296ab004d4e2df22
Jacob McCann <jacob.mccann2@target.com>
2015-11-17 16:51:44 -0600

    Generate Dockerfiles based on Chef version to build for

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-chefver=12.5.1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CHEF_VER=12.5.1
+if [ ! -z $1 ]; then
+  CHEF_VER=$1
+fi
+
+echo "Building containers for Chef $CHEF_VER"
 
 for platform in $(ls -d */ | tr -d /)
 do
@@ -9,11 +15,16 @@ do
   for version in $(ls -d */ | tr -d /)
   do
     cd $DIR/$platform/$version
-    if [ ! -f Dockerfile ]; then
+    if [ ! -f Dockerfile.template ]; then
       continue
     fi
-    echo "Building for $platform-$version"
-    docker build --rm=true -t jmccann/chef:${platform}-${version} .
-    docker build --rm=true -t jmccann/chef:${platform}-${version}-${chefver} .
+    echo "  building $platform-$version"
+    sed -e "s/CHEF_VER/${CHEF_VER}/g" Dockerfile.template > Dockerfile.${CHEF_VER}
+    docker build -f Dockerfile.${CHEF_VER} --rm=true -t jmccann/chef:${platform}-${version} .
+    if [ $? -ne 0 ]; then
+      echo "ERROR: Failed to build $platform-$version"
+      exit 1
+    fi
+    docker build -f Dockerfile.${CHEF_VER} --rm=true -t jmccann/chef:${platform}-${version}-${CHEF_VER} .
   done
 done
